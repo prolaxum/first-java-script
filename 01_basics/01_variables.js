86304446926a9c62782c16fa10ea7bab9d7c6188
prolaxum <158695602+prolaxum@users.noreply.github.com>
2025-12-13 14:56:17 +0000

    container config is done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 114565
+let accountEmail = "simarpalsingh215@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "567583"
+accountCity = "Delhi"
+
+// accountId = 2  // not allowed
+
+/* Prefer not to use var
+  because of block scope and functional scope */
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState]);
