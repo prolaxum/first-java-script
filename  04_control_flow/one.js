aacdc61de56e4efb3739ad9df9009f8a5dc6a89e
prolaxum <158695602+prolaxum@users.noreply.github.com>
2026-03-06 07:33:21 +0000

    container config

---
+++  04_control_flow/one.js
@@ -0,0 +1,8 @@
+// if
+const isUserLoggedIn = true
+if (2 != 3) {      // if the cindition is true then only the code in the scope will get executed
+   console.log("executed");
+   
+}
+
+// <, >, == , 
